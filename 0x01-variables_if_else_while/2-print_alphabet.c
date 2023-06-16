3ff52691d26b139e4e6b1121a5faa9c628d629dc
letiscia <letisciaonuselogu@gmail.com>
2023-06-16 15:38:11 -0700

    '

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: This function prints out a generated
+  *		random number
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	/* your code goes there */
