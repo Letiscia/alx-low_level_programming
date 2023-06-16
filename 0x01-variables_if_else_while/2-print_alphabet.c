b0285ac62288658eed3f9096950477cb65fc8855
letiscia <letisciaonuselogu@gmail.com>
2023-06-16 16:00:31 -0700

    '

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,10 @@
   */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last = n % 10;
-	/* your code goes there */
+	char value = 'a';
+	do {
+		putchar(value);
+		value++;
+	} while ("value <= 'z'\n");
+	return (0);
+}
