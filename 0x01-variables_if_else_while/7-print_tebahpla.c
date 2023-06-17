c541c8c37a60a89e33288f51e0d5045da1bce8d4
letiscia <letisciaonuselogu@gmail.com>
2023-06-16 19:36:22 -0700

    '

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: This function prints out a generated
+  *             random number
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha <= 'a'; alpha++)
+	{putchar(alpha); }
+	putchar('\n');
+	return (0);
+}
