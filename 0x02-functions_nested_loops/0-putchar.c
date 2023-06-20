eba9382ded8302247496b722d17af31b2dd67134
letiscia <letisciaonuselogu@gmail.com>
2023-06-20 11:22:35 -0700

    '

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print _putchar
+ * _putchar - function prototype
+ * @c: The character to print
+ *
+ * Return: 0
+ */
+int _putchar(char c);
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+-----C PROGRAMMING LANGUAGE-------
+
+         FUNCTIONS & NESTED LOOPS
