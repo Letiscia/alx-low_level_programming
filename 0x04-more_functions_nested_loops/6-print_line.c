82bce94809b21b53964d2e3825ca6143bfbb3ce7
Letiscia <73739194+Letiscia@users.noreply.github.com>
2023-06-22 23:47:43 +0100

    Create a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints the number of line
+ * @n: number of times to print line
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a = 1;
+
+	while (a <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
