f5a6c6c8f3a69fb61c0415077848c046a596fb88
Letiscia <73739194+Letiscia@users.noreply.github.com>
2023-06-22 23:34:05 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 void more_numbers(void)
 {
 	int a;
+	int b;
 
-	for (a = 0; a <= 14; a++)
+	for (a = 0; a < 10; a++)
 	{
-		int b;
-
 		for (b = 0; b < 15; b++)
 		{
 			if (b >= 10)
