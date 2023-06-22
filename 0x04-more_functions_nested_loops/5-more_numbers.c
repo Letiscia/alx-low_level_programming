829db4ed8c30b64d18eb02ff8a55b76aa52fb5dd
letiscia <letisciaonuselogu@gmail.com>
2023-06-22 15:25:13 -0700

    '

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * 
+ */
+void more_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 14; a++)
+	{
+		int b;
+
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
