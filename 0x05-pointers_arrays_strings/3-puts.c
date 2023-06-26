6c21b0776a350fba4e33af00eaedbbe8291ec60b
letiscia <letisciaonuselogu@gmail.com>
2023-06-26 13:38:22 -0700

    PRINT A STRING

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints out a string.
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+_puts(char *str)
+{
+		char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+
+	return (0);
+}
