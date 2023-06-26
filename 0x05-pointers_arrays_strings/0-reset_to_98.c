9d779c89e8c85873454af4833c39ee0aae957ff4
letiscia <letisciaonuselogu@gmail.com>
2023-06-26 11:28:12 -0700

    CHANGE FROM 402 TO 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,11 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * reset_to_98 - function that takes a pointer to an int as 
- * parameter and updates the value it points to to 98 
+ * reset_to_98 - function that takes a pointer to an int as parameter 
+ * and updates the value it points to to 98.
  *
  * Return: Always 0.
  */
-int main(void)
-{
-   void reset_to_98(int *n);
-   int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    *n = 98;
-    printf("n=%d\n", n);
-    return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
