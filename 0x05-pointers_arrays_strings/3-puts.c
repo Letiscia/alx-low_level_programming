f341d4ac40e17fdb7a5b03a5038313065e717287
letiscia <letisciaonuselogu@gmail.com>
2023-06-26 16:08:52 -0700

    PRINT TO STDOUT

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include <string.h>
 #include "main.h"
 /**
- * _puts - to print a string.
- * @str: string
+ * _puts - print string
+ * @str: string to print
  */
 void _puts(char *str)
 {
-	unsigned int i;
+	unsigned int a;
 
-	for (i = 0; i < strlen(str); i++)
+	for (a = 0; a < strlen(str); a++)
 	{
-		_putchar(str[i]);
+		_putchar(str[a]);
 	}
 	_putchar('\n');
 }
