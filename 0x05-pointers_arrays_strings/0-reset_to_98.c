e19e563394e42fecb95f192d70f417c565d16ac7
letiscia <letisciaonuselogu@gmail.com>
2023-06-26 11:33:11 -0700

    CHANGE FROM 402 TO 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter 
+ * reset_to_98 - function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
+ * @n: pointer
  *
  * Return: Always 0.
  */
