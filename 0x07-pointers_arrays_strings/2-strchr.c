88e579c431efdd3f52d3454b89a584293de14524
letiscia <letisciaonuselogu@gmail.com>
2023-07-04 16:06:45 -0700

    STRING CHARACTER

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,14 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	return ('\0');
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+	i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+
+		else
+			return (NULL);
 }
