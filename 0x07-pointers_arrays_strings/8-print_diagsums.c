946ca463cf612ed9aec5e2832877199bef9c5604
letiscia <letisciaonuselogu@gmail.com>
2023-07-04 15:49:41 -0700

    DIAGONALS

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,6 @@ void print_diagsums(int *a, int size)
 
 	sum1 = 0;
 	sum2 = 0;
-for (j = 0; j < size; j++)
 	{
 		i = (j * size) + j;
 		sum1 += a[i];
@@ -21,7 +20,7 @@ for (j = 0; j < size; j++)
 	for (j = 1; j <= size; j++)
 	{
 		i = (j * size) - j;
-			sum2 += a[i];
+		sum2 += a[i];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
