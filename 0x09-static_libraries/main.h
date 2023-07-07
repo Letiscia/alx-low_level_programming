3e7961bd22c241d9d3154d4b093803d589869b52
letiscia <letisciaonuselogu@gmail.com>
2023-07-07 06:08:58 -0700

    '

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -11,7 +10,6 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
