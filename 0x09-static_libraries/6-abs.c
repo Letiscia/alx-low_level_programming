f9e9a945dc46030e9b59f73ba35d26bdd23c77da
letiscia <letisciaonuselogu@gmail.com>
2023-07-07 06:04:00 -0700

    '

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: number variable
+ *
+ * Return: Always number
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
