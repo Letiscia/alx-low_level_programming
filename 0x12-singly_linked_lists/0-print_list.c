e6ef7b2b6ba912c6fe488748d1a9402573f50b6c
Letiscia <letisciaonuselogu@gmail.com>
2023-07-27 17:59:46 -0700

    LINKED LISTS

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,11 +19,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (!h->str)
-			printf("[%d] %s\n", 0, "(nil)");
+
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		mylist++;
 	}
 
-	return (mylist);	
+	return (mylist);
+}	
