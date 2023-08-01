365af75526fdb5fd5989e242021ebb98a3d2b985
Letiscia <letisciaonuselogu@gmail.com>
2023-08-01 16:12:46 -0700

    PRINT

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,12 @@ size_t print_listint(const listint_t *h)
 {
 	int num = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		num++;
 	}
-	printf("%d\n", h->n);
 
 	return (num);
 }
