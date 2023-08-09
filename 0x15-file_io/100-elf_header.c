da59f2682481e8670251780381d57375db72eb3e
Letiscia <letisciaonuselogu@gmail.com>
2023-08-08 18:21:48 -0700

    '

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,7 +6,7 @@
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
+void elf_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
